8ab85cf14dfb4f5d48e49a2697c9d0663e86e5db
Chraeten Lategan <chraetenlategan@gmail.com>
2026-08-17 17:17:04 +0200

    update changes

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ fetch("navbar.html")
     .then(data => {
         document.getElementById("navbar-placeholder").innerHTML = data
     })
+
+    fetch("footer.html")
+    .then(Response => Response.text())
+    .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data
+    })
